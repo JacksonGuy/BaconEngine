d2ee1427d6c78907d66a5790c776adc74a51fbd4
JacksonGuy <jacksonf955@gmail.com>
2024-07-09 00:15:44 -0400

    Removed assets

--- assets/scripts/testing.lua
+++
@@ -1,8 +0,0 @@
-if get_input_single("T") then
-    current = get_sprite()
-    if current == "./assets/player.jpg" then
-        set_sprite("./assets/player2.png")
-    elseif current == "./assets/player2.png" then
-        set_sprite("./assets/player.jpg")
-    end
-end
